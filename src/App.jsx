176d6a665d5036b16ff78627f13cb708743eaa35
Gerard Camí <dev.cxmi@gmail.com>
2024-04-28 10:20:38 +0200

    Day2

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,56 @@ import * as React from 'react'
 import './App.css'
 
 // Global variable will not be reloaded when the component is re-rendered
-const title = 'React'
+
+/* const greeting = 'Hello'
+const title = 'React' */
+
+/* const welcome = {
+  greeting: 'Hey',
+  title: 'React',
+} */
+
+/* function getTitle(title){
+  return title;
+} */
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  }
+]
 
 function App() {
-  
-
   return (
     <div>
-      <h1>Hello {title}</h1>
+      <h1>
+        {/* {welcome.greeting} {welcome.title} */}
+        {/* Hello {getTitle('React')} */}
+        My Hacker Stories
+        </h1>
+      <label htmlFor="search">Search: </label>
+      <input id='search' type="text" />
+
+      <hr />
+
+      <ul>
+        {list.map(function (item) {
+          return <li>{item.title}</li>
+        })}
+      </ul>
     </div>
   )
 }
