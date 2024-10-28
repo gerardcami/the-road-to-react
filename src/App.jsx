637f4fad3d02ea9505d10bad93913f3780cb921d
gcami <gerardcamigay@gmail.com>
2024-10-28 13:01:16 +0100

    Last chapter: React State

--- src/App.jsx
+++ src/App.jsx
@@ -15,26 +15,25 @@ const title = 'React' */
   return title;
 } */
 
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
-function App() {
+const App = () => {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
   return (
     <div>
       <h1>
@@ -42,25 +41,13 @@ function App() {
         {/* Hello {getTitle('React')} */}
         My Hacker Stories
       </h1>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+
+      <Search />
 
       <hr />
 
-      <ul>
-        {list.map(function (item) {
-          return (
-            <li key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-            </li>
-          );
-        })}
-      </ul>
+      <List list={stories} />
+
       {/* Use the index of the element as the key. Only use it as last resort */}
       {/* <ul>
         {list.map(function (item, index) {
@@ -69,6 +56,57 @@ function App() {
       </ul> */}
     </div>
   );
-}
+};
+
+const Search = () => {
+  const [searchTerm, setSearchTerm] = React.useState("");
+
+  /* Event handler example in JSX */
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleBlur = (event) => {
+    console.log(event);
+    console.log(event.target.value);
+  };
+
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      {/* Add the event handler as an attribute to the element in JSX with onChange */}
+      <input
+        id="search"
+        type="text"
+        onChange={handleChange}
+        onBlur={handleBlur}
+      />
+
+      <p>
+        Searching for <strong>{searchTerm}</strong>
+      </p>
+    </div>
+  );
+};
+
+/* Arrow function example */
+const List = (props) => (
+  <ul>
+    {props.list.map((item) => {
+      return <Item item={item} />;
+    })}
+  </ul>
+);
+
+const Item = (props) => (
+  <li key={props.item.objectID}>
+    <span>
+      <a href={props.item.url}>{props.item.title}</a>
+    </span>
+    <span>{props.item.author}</span>
+    <span>{props.item.num_comments}</span>
+    <span>{props.item.points}</span>
+  </li>
+);
 
 export default App;
